2447078f57a6f401f6acc9effdb5706067b5c9f4
tatiana.voitekhovitch <tatsiana.sarnatskaya@gmail.com>
2024-02-06 01:41:30 +0100

    fix commit

--- src/com/tatchik/cs/homework3/Task3Arrays.java
+++ src/com/tatchik/cs/homework3/Task3Arrays.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 /**
  * Дан одномерный массив целых чисел.
- * Написать функцию, которая принимает этот массив и разбивает на 3 других: с только отрицательными числами, только положительными и только нули.
+ * Написать функцию, которая принимает этот массив и разбивает на 3 других: с только отрицательными числами,
+ * только положительными и только нули.
  * Если для какого-то из массивов не будет значений, то должен быть создан пустой массив.
  * Возвращает функция эти три массива в виде одного двумерного.
  * Пример:
@@ -20,8 +21,10 @@ public class Task3Arrays {
     public static void main(String[] args) {
         int[] inputArray = {-4, 0, 1, 9, 0, -18, 3};
 
+        // Вызываем функцию для разбиения массива
         int[][] resultArrays = splitArray(inputArray);
 
+        // Выводим результаты
         System.out.println("Исходный массив: " + Arrays.toString(inputArray));
         System.out.println("Отрицательные числа: " + Arrays.toString(resultArrays[0]));
         System.out.println("Нули: " + Arrays.toString(resultArrays[1]));
@@ -30,111 +33,62 @@ public class Task3Arrays {
 
     /**
      * Функция для разбиения массива на три других: с отрицательными числами, нулями и положительными числами.
-     *
      * @param inputArray Исходный массив целых чисел
      * @return Двумерный массив, содержащий три массива: с отрицательными числами, нулями и положительными числами
      */
     public static int[][] splitArray(int[] inputArray) {
-        int countNegatives = countNegatives(inputArray);
-        int countZeros = countZeros(inputArray);
-        int countPositives = countPositives(inputArray);
+        // Вычисляем количество отрицательных, нулевых и положительных чисел в массиве
+        int[] counts = countNumbers(inputArray);
 
-        int[] negativesArray = createNegativesArray(inputArray, countNegatives);
-        int[] zerosArray = createZerosArray(inputArray, countZeros);
-        int[] positivesArray = createPositivesArray(inputArray, countPositives);
+        // Создаем три массива с соответствующими значениями
+        int[] negativesArray = createArray(inputArray, counts[0], -1);
+        int[] zerosArray = createArray(inputArray, counts[1], 0);
+        int[] positivesArray = createArray(inputArray, counts[2], 1);
 
+        // Возвращаем двумерный массив
         return new int[][]{negativesArray, zerosArray, positivesArray};
     }
 
     /**
-     * Подсчитывает количество отрицательных чисел в массиве.
+     * Подсчитывает количество отрицательных, нулевых и положительных чисел в массиве.
      * @param array Массив целых чисел
-     * @return Количество отрицательных чисел
+     * @return Массив из трех элементов: количество отрицательных, нулевых и положительных чисел
      */
-    private static int countNegatives(int[] array) {
-        int count = 0;
-        for (int num : array) {
+    private static int[] countNumbers(int[] array) {
+        int[] counts = new int[3];
+
+        for (int i = 0; i < array.length; i++) {
+            int num = array[i];
             if (num < 0) {
-                count++;
+                counts[0]++;
+            } else if (num == 0) {
+                counts[1]++;
+            } else {
+                counts[2]++;
             }
         }
-        return count;
+
+        return counts;
     }
 
     /**
-     * Подсчитывает количество нулевых чисел в массиве.
-     * @param array Массив целых чисел
-     * @return Количество нулевых чисел
-     */
-    private static int countZeros(int[] array) {
-        int count = 0;
-        for (int num : array) {
-            if (num == 0) {
-                count++;
-            }
-        }
-        return count;
-    }
-    /**
-     * Подсчитывает количество положительных чисел в массиве.
-     * @param array Массив целых чисел
-     * @return Количество положительных чисел
-     */
-    private static int countPositives(int[] array) {
-        int count = 0;
-        for (int num : array) {
-            if (num > 0) {
-                count++;
-            }
-        }
-        return count;
-    }
-    /**
-     * Создает массив отрицательных чисел из исходного массива.
+     * Создает массив чисел (отрицательных, нулевых или положительных) из исходного массива.
+     *
      * @param array Массив целых чисел
      * @param size  Размер создаваемого массива
-     * @return Массив отрицательных чисел
+     * @param sign  Знак, определяющий тип чисел (отрицательные, нулевые или положительные)
+     * @return Массив чисел (отрицательных, нулевых или положительных)
      */
-    private static int[] createNegativesArray(int[] array, int size) {
-        int[] negativesArray = new int[size];
+    private static int[] createArray(int[] array, int size, int sign) {
+        int[] resultArray = new int[size];
         int index = 0;
-        for (int num : array) {
-            if (num < 0) {
-                negativesArray[index++] = num;
+
+        for (int i = 0; i < array.length; i++) {
+            int num = array[i];
+            if ((num * sign) > 0) {
+                resultArray[index++] = num;
             }
         }
-        return negativesArray;
-    }
-    /**
-     * Создает массив нулевых чисел из исходного массива.
-     * @param array Массив целых чисел
-     * @param size  Размер создаваемого массива
-     * @return Массив нулевых чисел
-     */
-    private static int[] createZerosArray(int[] array, int size) {
-        int[] zerosArray = new int[size];
-        int index = 0;
-        for (int num : array) {
-            if (num == 0) {
-                zerosArray[index++] = num;
-            }
-        }
-        return zerosArray;
-    }
-    /**
-     * Создает массив положительных чисел из исходного массива.
-     * @param array Массив целых чисел
-     * @param size  Размер создаваемого массива
-     * @return Массив положительных чисел
-     */
-    private static int[] createPositivesArray(int[] array, int size) {
-        int[] positivesArray = new int[size];
-        int index = 0;
-        for (int num : array) {
-            if (num > 0) {
-                positivesArray[index++] = num;
-            }
-        }
-        return positivesArray;
+        return resultArray;
     }
 }
