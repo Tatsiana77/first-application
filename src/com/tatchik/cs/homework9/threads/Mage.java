f86903602783f09b267129e888029a3d7968f7d1
tatiana.voitekhovitch <tatsiana.sarnatskaya@gmail.com>
2024-03-19 19:12:58 +0100

    fixed commit homework9

--- src/com/tatchik/cs/homework9/threads/Mage.java
+++ src/com/tatchik/cs/homework9/threads/Mage.java
@@ -22,10 +22,22 @@ public class Mage extends Thread {
             List<Crystal> newCrystals = Planet.generateCrystals();
             collectedCrystals.addAll(newCrystals);
 
+            // Вывод текущего состояния сбора кристаллов магом.
+            printCollectedCrystalsCount();
+            // Проверяем, собрал ли маг необходимое количество кристаллов для победы и не завершена ли гонка.
             if (hasCollectedRequiredCrystals() && !raceFinished) {
+                // Используем синхронизированный блок на уровне класса Mage, чтобы гарантировать,
+                // что в данный момент времени только один поток может проверить и изменить состояние гонки.
                 synchronized (Mage.class) {
+                    // Повторно проверяем, не завершена ли гонка, чтобы избежать ситуации, когда два мага
+                    // одновременно достигают условий победы и пытаются объявить себя победителями.
                     if (!raceFinished) {
-                        System.out.println(race + " mage has collected the required crystals and won the race!");
+                        // Выводим в консоль сообщение о победе мага данной расы.
+                        System.out.println("\033[31m"  +  race + " \033[0m"
+                                + " mage has collected the required crystals and  " + "\033[31m"  + " WON " +  " \033[0m"
+                                + "the race!");
+                        // Устанавливаем флаг raceFinished в true, чтобы указать на завершение гонки
+                        // и предотвратить дальнейшие объявления победителя.
                         raceFinished = true;
                     }
                 }
@@ -41,9 +53,36 @@ public class Mage extends Thread {
     }
 
     private boolean hasCollectedRequiredCrystals() {
-        long redCount = collectedCrystals.stream().filter(c -> c.getType() == Crystal.Type.RED).count();
-        long whiteCount = collectedCrystals.stream().filter(c -> c.getType() == Crystal.Type.WHITE).count();
-        return redCount >= GOAL && whiteCount >= GOAL;
+//        long redCount = collectedCrystals.stream().filter(c -> c.getType() == Crystal.Type.RED).count();
+//        long whiteCount = collectedCrystals.stream().filter(c -> c.getType() == Crystal.Type.WHITE).count();
+//        return redCount >= GOAL && whiteCount >= GOAL;
+
+        long redCount = 0;
+        long whiteCount = 0;
+       for (Crystal crystal : collectedCrystals) {
+          if (crystal.getType() == Crystal.Type.RED) {
+               redCount++;
+           }
+         else if (crystal.getType() == Crystal.Type.WHITE) {
+                whiteCount++;
+            }
+       }
+      return redCount >= GOAL && whiteCount >= GOAL;
+    }
+
+    private void printCollectedCrystalsCount() {
+        long redCount = 0;
+        long whiteCount = 0;
+
+        for (Crystal crystal : collectedCrystals) {
+            if (crystal.getType() == Crystal.Type.RED) {
+                redCount++;
+            } else if (crystal.getType() == Crystal.Type.WHITE) {
+                whiteCount++;
+            }
+        }
+
+        System.out.println(race + " mage has collected " + redCount + " red crystals and " + whiteCount + " white crystals.");
     }
 }
 
